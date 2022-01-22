f6074a43aeaffe26d23ec2391ed5bf4982330dea
srivatsav06 <srivatsav184536@gmail.com>
2022-01-22 17:47:17 +0530

    this is for jinx

---
+++ Hello.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Hello
+{
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World");
+    }
+}
